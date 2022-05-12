60709aa1e5ecb23c913c28be8aa7d4e7d37922a1
ograciano <cev3067@innovaccion.mx>
2022-05-11 19:39:05 -0500

    Paso 6 Creacion CRUD completo de la tabla Explorers

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,79 +2,79 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 (async function main() {
-  try {
-    const woopa = await prisma.explorer.upsert({
-      where: { name: 'Woopa' },
-      update: {},
-      create: {
-        name: 'Woopa',
-				username: 'ajolonauta',
-				mission: 'Node'
-      },
-    });
+    try {
+        const woopa = await prisma.explorer.upsert({
+            where: { name: 'Woopa' },
+            update: {},
+            create: {
+                name: 'Woopa',
+                username: 'ajolonauta',
+                mission: 'Node'
+            },
+        });
 
-    const woopa1 = await prisma.explorer.upsert({
-      where: { name: 'Woopa1' },
-      update: {},
-      create: {
-        name: 'Woopa1',
-				username: 'ajolonauta1',
-				mission: 'Node'
-      },
-    });
+        const woopa1 = await prisma.explorer.upsert({
+            where: { name: 'Woopa1' },
+            update: {},
+            create: {
+                name: 'Woopa1',
+                username: 'ajolonauta1',
+                mission: 'Node'
+            },
+        });
 
-    const woopa2 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 2' },
-      update: {},
-      create: {
-        name: 'Woopa 2',
-				username: 'ajolonauta2',
-				mission: 'Java'
-      },
-    });
+        const woopa2 = await prisma.explorer.upsert({
+            where: { name: 'Woopa 2' },
+            update: {},
+            create: {
+                name: 'Woopa 2',
+                username: 'ajolonauta2',
+                mission: 'Java'
+            },
+        });
 
-    const woopa3 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 3' },
-      update: {},
-      create: {
-        name: 'Woopa 3',
-				username: 'ajolonauta3',
-				mission: 'Node'
-      },
-    });
-    const woopa4 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 4' },
-      update: {},
-      create: {
-        name: 'Woopa 4',
-				username: 'ajolonauta4',
-				mission: 'Node'
-      },
-    });
-    const woopa5 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 5' },
-      update: {},
-      create: {
-        name: 'Woopa 5',
-				username: 'ajolonauta5',
-				mission: 'Node'
-      },
-    });
-    const woopa6 = await prisma.explorer.upsert({
-      where: { name: 'Woopa 6' },
-      update: {},
-      create: {
-        name: 'Woopa 6',
-				username: 'ajolonauta6',
-				mission: 'Node'
-      },
-    });
+        const woopa3 = await prisma.explorer.upsert({
+            where: { name: 'Woopa 3' },
+            update: {},
+            create: {
+                name: 'Woopa 3',
+                username: 'ajolonauta3',
+                mission: 'Node'
+            },
+        });
+        const woopa4 = await prisma.explorer.upsert({
+            where: { name: 'Woopa 4' },
+            update: {},
+            create: {
+                name: 'Woopa 4',
+                username: 'ajolonauta4',
+                mission: 'Node'
+            },
+        });
+        const woopa5 = await prisma.explorer.upsert({
+            where: { name: 'Woopa 5' },
+            update: {},
+            create: {
+                name: 'Woopa 5',
+                username: 'ajolonauta5',
+                mission: 'Node'
+            },
+        });
+        const woopa6 = await prisma.explorer.upsert({
+            where: { name: 'Woopa 6' },
+            update: {},
+            create: {
+                name: 'Woopa 6',
+                username: 'ajolonauta6',
+                mission: 'Node'
+            },
+        });
 
-    console.log('Create 7 explorers');
-  } catch(e) {
-    console.error(e);
-    process.exit(1);
-  } finally {
-    await prisma.$disconnect();
-  }
+        console.log('Create 7 explorers');
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    } finally {
+        await prisma.$disconnect();
+    }
 })();
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {PrismaClient} = require('@prisma/client');
+const { response } = require('express');
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,64 @@ app.get('/', (req, res) => {
     res.json({message: 'API CRUD'});
 });
 
+app.get('/explorers', async (req, res=response) => {
+    try {
+        const allExplorers = await prisma.explorer.findMany({});
+        res.json(allExplorers);
+    } catch (e) {
+        console.log(e);
+        res.json({message: 'Error en el servidor'})
+    }
+})
 
+app.get('/explorers/:id', async (req, res=response) => {
+    try {
+        const {id} = req.params;
+        const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+        res.json(explorer);
+    } catch (e) {
+        console.log(e);
+        res.json({message: 'Error en el servidor'})
+    }
+});
+
+app.post('/explorers', async(req, res=response) => {
+    try {
+        const {name, username, mission} = req.body;
+        const explorer = {name, username, mission};
+        const message = 'Explorer Creado';
+        await prisma.explorer.create({data:explorer});
+        return res.json({message});
+    } catch (e) {
+        console.log(e);
+        res.json.status(500).json({message: 'Error en el servidor'});
+    }
+});
+
+app.put('/explorers/:id', async(req, res=response) => {
+    try {
+        const {id} = req.params;
+        const {name, username, mission} = req.body;
+        const explorer = {name, username, mission};
+        const message = 'Explorer Actualizado';
+        await prisma.explorer.update({where: {id: parseInt(id)}, data:explorer});
+        return res.json({message});
+    } catch (e) {
+        console.log(e);
+        res.json.status(500).json({message: 'Error en el servidor'});
+    }
+});
+
+app.delete('/explorers/:id', async(req, res=response) => {
+    try {
+        const {id} = req.params;
+        const message = 'Explorer Eliminado';
+        await prisma.explorer.delete({where: {id: parseInt(id)}});
+        return res.json({message});
+    } catch (e) {
+        console.log(e);
+        res.json.status(500).json({message: 'Error en el servidor'});
+    }
+});
 
 app.listen(port, () => { console.log(`Escuchando servidor en el puerto ${port}`);});
