268587a28a19f01046d4a77f46e7571e4b18cdf7
ograciano <cev3067@innovaccion.mx>
2022-05-12 08:46:40 -0500

    Paso 9 Instalar dependencia cors y configurarla en el proyecto

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const {PrismaClient} = require('@prisma/client');
 const { response } = require('express');
+const cors = require('cors');
 
+const corsOptions = {origin: "http://localhost:8081"};
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3000;
 
@@ -66,6 +69,66 @@ app.delete('/explorers/:id', async(req, res=response) => {
         const {id} = req.params;
         const message = 'Explorer Eliminado';
         await prisma.explorer.delete({where: {id: parseInt(id)}});
+        return res.json({message});
+    } catch (e) {
+        console.log(e);
+        res.json.status(500).json({message: 'Error en el servidor'});
+    }
+});
+
+app.get('/ajolonautas', async (req, res=response) => {
+    try {
+        const allAjolonautas = await prisma.ajolonauta.findMany({});
+        res.json(allAjolonautas);
+    } catch (e) {
+        console.log(e);
+        res.json({message: 'Error en el servidor'})
+    }
+})
+
+app.get('/ajolonautas/:id', async (req, res=response) => {
+    try {
+        const {id} = req.params;
+        const ajolonauta = await prisma.ajolonauta.findUnique({where: {id: parseInt(id)}});
+        res.json(ajolonauta);
+    } catch (e) {
+        console.log(e);
+        res.json({message: 'Error en el servidor'})
+    }
+});
+
+app.post('/ajolonautas', async(req, res=response) => {
+    try {
+        const {name, lang, missionCommander} = req.body;
+        const ajolonauta = {name, lang, missionCommander};
+        const message = 'Ajolonauta Creado';
+        await prisma.ajolonauta.create({data:ajolonauta});
+        return res.json({message});
+    } catch (e) {
+        console.log(e);
+        res.json.status(500).json({message: 'Error en el servidor'});
+    }
+});
+
+app.put('/ajolonautas/:id', async(req, res=response) => {
+    try {
+        const {id} = req.params;
+        const {name, lang, missionCommander} = req.body;
+        const ajolonauta = {name, lang, missionCommander};
+        const message = 'Ajolonauta Actualizado';
+        await prisma.ajolonauta.update({where: {id: parseInt(id)}, data:ajolonauta});
+        return res.json({message});
+    } catch (e) {
+        console.log(e);
+        res.json.status(500).json({message: 'Error en el servidor'});
+    }
+});
+
+app.delete('/ajolonautas/:id', async(req, res=response) => {
+    try {
+        const {id} = req.params;
+        const message = 'Ajolonauta Eliminado';
+        await prisma.ajolonauta.delete({where: {id: parseInt(id)}});
         return res.json({message});
     } catch (e) {
         console.log(e);
